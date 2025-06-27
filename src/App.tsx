2f809fbd4db099b661ada35614171ad8ca7b6420
otisdoan <otisdoan@icloud.com>
2025-06-27 10:10:29 +0700

    set up more structure folder project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+/* eslint-disable no-debugger */
 import "./App.css";
 
 function App() {
+  const arr = ["Le Doan Hieu", "A", "B", "C"];
+  arr.forEach((element) => {
+    console.log(element);
+  });
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 

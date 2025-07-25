357b9019838eaeb969306f82d58064569639400d
otisdoan <ledoanhieu12a6@gmail.com>
2025-07-25 15:49:23 +0700

    remve set token

--- src/guards/AuthGuard.tsx
+++ src/guards/AuthGuard.tsx
@@ -1,12 +1,12 @@
 import type React from "react";
-import { Navigate, useLocation } from "react-router-dom";
+// import { Navigate, useLocation } from "react-router-dom";
 
 const AuthGuard = ({ children }: { children: React.JSX.Element }) => {
-  const accessToken = localStorage.getItem("access_token");
-  const location = useLocation();
-  if (!accessToken || accessToken === "undefined" || accessToken === "null") {
-    return <Navigate to={"/login"} state={{ from: location }} replace />;
-  }
+  // const accessToken = localStorage.getItem("access_token");
+  // const location = useLocation();
+  // if (!accessToken || accessToken === "undefined" || accessToken === "null") {
+  //   return <Navigate to={"/login"} state={{ from: location }} replace />;
+  // }
 
   return children;
 };

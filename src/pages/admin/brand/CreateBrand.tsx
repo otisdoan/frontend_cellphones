8bb5c8c13b06c8113f9155817d09f9c1ee35b927
otisdoan <ledoanhieu12a6@gmail.com>
2025-07-28 21:48:01 +0700

    font size text

--- src/pages/admin/brand/CreateBrand.tsx
+++ src/pages/admin/brand/CreateBrand.tsx
@@ -20,14 +20,14 @@ const CreateBrand = () => {
   return (
     <>
       <div className="p-4">
-        <div className="md:flex items-center justify-between">
+        <div className="md:flex items-center justify-between mb-[2rem]">
           <div>
             <div className="flex items-center gap-x-1">
               <MdOutlineArrowBack
-                className="text-[1.5rem] cursor-pointer"
+                className="md:text-[1.5rem] text-[1.3rem] cursor-pointer"
                 onClick={() => navigate(-1)}
               />
-              <h1 className="md:font-medium text-[1.7rem] md:block">
+              <h1 className="md:font-medium md:text-[1.7rem] text-[1.2rem] md:block">
                 Create new brand
               </h1>
             </div>
